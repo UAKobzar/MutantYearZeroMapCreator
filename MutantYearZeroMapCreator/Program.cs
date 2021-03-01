bbcc862a90253212f0ed2b0d5cae4804869d4bf8
Valentine_Karpenko <valentyn_karpenko@constructsecure.com>
2021-03-01 15:37:41 +0200

    Add project files.

---
+++ MutantYearZeroMapCreator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MutantYearZeroMapCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
